f98f68678a6ae782c772bd81f1e74ebd05531beb
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2022-03-09 16:31:21 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+jQuery(document).ready(function(){
+$(function (){
+    $(".Soglsovon").click(function(){
+    $(".Drop-block").slideToggle(500);
+    $(".Drop-downt-text").css("border-radius", "0px");
+});
+});
+});
